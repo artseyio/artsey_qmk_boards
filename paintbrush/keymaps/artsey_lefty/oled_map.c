b5d54a9175cb9742c3281ba05fc1b4c32240fa29
carcinization <80171031+carcinization@users.noreply.github.com>
2021-06-12 21:04:42 -0400

    Fixed Lefty Brackets Layer Map

--- paintbrush/keymaps/artsey_lefty/oled_map.c
+++ paintbrush/keymaps/artsey_lefty/oled_map.c
@@ -15,10 +15,10 @@ const char sym_map[]= "`;\\!\n"
 "=-?  ";
 const char shifted_sym_map[]= "~:|!\n"
 "+_?  ";
-const char brac_map[]= "{)( \n"
-"}][} ";
-const char shifted_brac_map[]= "{)( \n"
-"}}{} ";
+const char brac_map[]= "}() \n"
+"{[]} ";
+const char shifted_brac_map[]= "}() \n"
+"{{}} ";
 static char const nav_map[] PROGMEM = {0x1E, 0x11, 0x18, 0x10, 0x20, 0x1F, 0x1B, 0x19, 0x1A, 0
 	};
 static char const mou_map[] PROGMEM = {0x1E, 0x32, 0x18, 0x31, 0x20, 0x1F, 0x1B, 0x19, 0x1A, 0
